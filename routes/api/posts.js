028d7af3c36573ecd74793ee5b085db95b9f9be2
Thuan Nguyen Phuong <nguyenphuongthuan95@gmail.com>
2019-03-11 01:26:48 +0700

    Get and Create Post API

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,7 +8,7 @@ const Post = require('../../models/Post');
 // @desc    Get All Posts
 // @access  Public
 router.get('/', async (req, res) => {
-  const posts = await Post.find();
+  const posts = await Post.find({});
   return res.json(posts);
 });
 
@@ -18,14 +18,11 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   logger.debug('request: ', req.params)
-
   try {
     const post = await Post.findById(id);
-    if (!post) return res.status(400).json({ message: "Post not found!" });
     return res.json(post);
-
   } catch (error) {
-    return res.status(400).json({ message: "Post not found!" });
+    return res.status(400).json({ msg: 'Post Does not exist' });
   }
 });
 
@@ -33,11 +30,13 @@ router.get('/:id', async (req, res) => {
 // @desc    Create A Post
 // @access  Private
 router.post('/', async (req, res) => {
+  logger.debug('request: ', req.body);
   const { title, content, description, fileUrl } = req.body;
 
   if (!content || !title) return res.status(400).json({ message: "Please enter all the required fields!"});
 
   const newPost = await new Post({
+    title,
     description,
     content,
     fileUrl
