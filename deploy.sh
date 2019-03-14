a16332f1add3c15dd8b03c2c31625957cda45c4b
Thuan Nguyen Phuong <nguyenphuongthuan95@gmail.com>
2019-03-14 22:04:02 +0700

    Add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
 #! /usr/bin/env bash
 
 set -e
-
+echo "ssh running"
 ssh -o StrictHostKeyChecking=no -i ~/Desktop/SSHKEY/npt.pem ec2-user@13.250.108.49 <<'ENDSSH'
 #commands to run on remote host
 cd /home/ec2-user/web-enterprise-backend
+
+echo "Pulling version control"
+git pull origin master
+
+echo "Building docker image"
 sudo docker build -t wep .
+
+echo "Recreate docker container"
 sudo docker-compose up -d --force-recreate
 ENDSSH
